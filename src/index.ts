1a308ac1ec724931ccf4a946eb5ce23b8723030a
João Pedro Silva <pedrojoaoti@gmail.com>
2022-12-19 18:49:30 -0300

    feat: add utils cpf and cnpj (#3)
    * feat: add functions
    
    * test: specs functions
    
    * feat: noused function
    
    * fix: yarn build
    
    * fix: change name folder
    
    * fix: change text

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 export { default as formatterMoney } from './utils/formatterMoney';
 
+// hooks
 export { default as useOnScreen } from './hooks/useOnScreen';
+
+// normalizers
+export { default as normalizeCpfOrCnpj } from './normalizers/normalizeCpfOrCnpj';
+
+// validations
+export { default as isCpf } from './validations/isCpf';
+export { default as isCnpj } from './validations/isCnpj';
+
