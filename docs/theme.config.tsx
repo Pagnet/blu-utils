03c59936e8fd3510e690dae775c7c59a68099107
João Pedro Silva <pedrojoaoti@gmail.com>
2022-12-20 21:33:34 -0300

    chore: setup doc project

---
+++ docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>Blu Utils</span>,
+  project: {
+    link: 'https://github.com/Pagnet/blu-utils',
+  },
+  docsRepositoryBase: 'https://github.com/Pagnet/blu-utils',
+  footer: {
+    text: 'Blu Pagamentos',
+  },
+};
+
+export default config;
