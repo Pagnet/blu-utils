9d2bae8b97559e637d098d06607e8899b387243c
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-01-18 18:26:36 -0300

    fix: rename functions from nit to pis

--- src/normalizers/normalizePis.ts
+++ src/normalizers/normalizePis.ts
@@ -1,6 +1,6 @@
 import maskString from '../utils/maskString';
 
-export default function normalizeNIT(value: string): string | boolean | undefined {
+export default function normalizePis(value: string): string | boolean | undefined {
   if (value.length === 11) {
     return maskString(value, '###.#####.##-#');
   }
