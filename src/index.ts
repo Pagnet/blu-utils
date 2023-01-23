98d0a595607f54db708d89c132ab7f571b1135a5
Ramon Cibas <63371945+ramoncibas@users.noreply.github.com>
2023-01-23 19:58:41 -0300

    feat: add function pis (#6)
    * feat: add imports functions
    
    * feat: add functions
    
    * test: specs functions
    
    * fix: rename functions from nit to pis
    
    Co-authored-by: João Pedro Silva <pedrojoaoti@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { default as useOnScreen } from './hooks/useOnScreen';
 export { default as normalizeMoney } from './normalizers/normalizeMoney';
 export { default as normalizeCpfOrCnpj } from './normalizers/normalizeCpfOrCnpj';
 export { default as normalizePhone } from './normalizers/normalizePhone';
+export { default as normalizePis } from './normalizers/normalizePis';
 
 // utils
 export { default as maskString } from './utils/maskString';
@@ -12,3 +13,4 @@ export { default as maskString } from './utils/maskString';
 // validations
 export { default as isCpf } from './validations/isCpf';
 export { default as isCnpj } from './validations/isCnpj';
+export { default as isPis } from './validations/isPis';
