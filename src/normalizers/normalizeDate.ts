802b0ee8ddaddebfbda4652d1043168010827789
ramoncibas <ramon.cibas@hotmail.com>
2023-03-20 10:46:16 -0300

    fix: lint

--- src/normalizers/normalizeDate.ts
+++ src/normalizers/normalizeDate.ts
@@ -1,36 +1,39 @@
 type Types = 'bigger' | 'long' | 'numeric';
 
 export default function normalizeDate(date: string, type: Types = 'numeric') {
-  const bigger = () => new Date(`${date}Z`).toLocaleString('pt-BR', {
-    weekday: 'long',
-    month: 'long',
-    year: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    timeZone: 'America/Sao_Paulo',
-    timeZoneName: 'long'
-  })
-    
-  const long = () => new Date(date).toLocaleDateString('pt-BR', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  })
+  const bigger = () =>
+    new Date(`${date}Z`).toLocaleString('pt-BR', {
+      weekday: 'long',
+      month: 'long',
+      year: 'numeric',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZone: 'America/Sao_Paulo',
+      timeZoneName: 'long',
+    });
 
-  const numeric = () => new Date(date).toLocaleDateString('pt-BR', {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric'
-  })
+  const long = () =>
+    new Date(date).toLocaleDateString('pt-BR', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+
+  const numeric = () =>
+    new Date(date).toLocaleDateString('pt-BR', {
+      year: 'numeric',
+      month: 'numeric',
+      day: 'numeric',
+    });
 
   const func: any = {
     bigger,
     long,
-    numeric
-  }
+    numeric,
+  };
 
   return func[type]();
 }
--- tests/normalizeDate.spec.ts
+++ tests/normalizeDate.spec.ts
@@ -4,19 +4,23 @@ import { normalizeDate } from '../src';
 
 describe('test normalizeDate', () => {
   test('spec normalize date types', () => {
-    expect(normalizeDate('2023-12-12 23:39:25.756', 'bigger'))
-      .toBe('terça-feira, 12 de dezembro de 2023 20:39:25 Horário Padrão de Brasília');
-    
-    expect(normalizeDate('2023-01-01 23:39:25.756', 'long'))
-        .toBe('domingo, 1 de janeiro de 2023');
-    
-    expect(normalizeDate('2023-01-16 23:39:25.756'))
-      .toBe('16/01/2023');
-    
-    expect(normalizeDate('2023-22-12 23:39:25.756', 'long'))
-      .toBe('Invalid Date');
+    expect(normalizeDate('2023-12-12 23:39:25.756', 'bigger')).toBe(
+      'terça-feira, 12 de dezembro de 2023 20:39:25 Horário Padrão de Brasília',
+    );
 
-    expect(normalizeDate('2023-01-01 23:39:25.756'))
-      .not.toBe('domingo, 1 de janeiro de 2023');
+    expect(normalizeDate('2023-01-01 23:39:25.756', 'long')).toBe(
+      'domingo, 1 de janeiro de 2023',
+    );
+
+    expect(normalizeDate('2023-01-16 23:39:25.756')).toBe('16/01/2023');
+
+    expect(normalizeDate('2023-22-12 23:39:25.756', 'long')).toBe(
+      'Invalid Date',
+    );
+
+    expect(normalizeDate('2023-01-01 23:39:25.756')).not.toBe(
+      'domingo, 1 de janeiro de 2023',
+    );
   });
 });
+
