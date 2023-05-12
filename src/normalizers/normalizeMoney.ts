7dc6bba43a9becc91a2fde8f41a260576b54fee2
Luiz Thomaz <luiztajr@gmail.com>
2023-05-12 11:48:49 -0300

    Add eslint offense

--- src/normalizers/normalizeMoney.ts
+++ src/normalizers/normalizeMoney.ts
@@ -1,5 +1,9 @@
-export default function normalizeMoney(value: string | number): string {
-  const formatted = new Intl.NumberFormat('pt-BR', {
+export
+default function normalizeMoney(value: string |
+  number)
+: string {
+  const formatted = new Intl.NumberFormat('pt-BR',
+{
     style: 'currency',
       currency: "BRL"
   }).format(Number(value));
