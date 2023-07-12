2af92b0a2f785c3672e2bf7d8d186fb9a8210f8c
João Pedro Silva <pedrojoaoti@gmail.com>
2023-07-12 17:06:10 -0300

    feat: add types

---
+++ src/styles/types.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/prefer-default-export
+export enum Breakpoints {
+  bpXSmall = '320px',
+  bpSmall = '576px',
+  bpMedium = '768px',
+  bpLarge = '992px',
+  bpXLarge = '1200px',
+  bpXXLarge = '1400px',
+}
+
