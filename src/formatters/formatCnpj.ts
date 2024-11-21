0df7ae1c6df2ee68788541466a386f1095ff8688
Leonardo Dourado <102989712+leonardodouradol@users.noreply.github.com>
2024-11-21 16:15:49 -0300

    feat: add formatter for cnpj (#30)

---
+++ src/formatters/formatCnpj.ts
@@ -0,0 +1,11 @@
+const formatCnpj = (value: string): string => {
+  const formattedValue = value.replace(/\D/g, '').slice(0, 14);
+
+  return formattedValue
+    .replace(/^(\d{2})(\d)/, '$1.$2')
+    .replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3')
+    .replace(/^(\d{2})\.(\d{3})\.(\d{3})(\d)/, '$1.$2.$3/$4')
+    .replace(/^(\d{2})\.(\d{3})\.(\d{3})\/(\d{4})(\d)/, '$1.$2.$3/$4-$5');
+};
+
+export default formatCnpj;
